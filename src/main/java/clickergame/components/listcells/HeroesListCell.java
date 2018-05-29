1ca97675bba36cb7115f3c8edeb01f342bfa5701
Alexandros Yiangou <alexyiangougaming@gmail.com>
2018-05-29 11:17:38 +0300

    Heroes-List-View - Improvements On Custom Hero List Cell

--- src/main/java/clickergame/components/listcells/HeroesListCell.java
+++ src/main/java/clickergame/components/listcells/HeroesListCell.java
@@ -6,31 +6,38 @@ package clickergame.components.listcells;
  */
 
 import javafx.scene.control.ListCell;
+import clickergame.models.Hero;
 import javafx.scene.Node;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Label;
 
-public class HeroesListCell extends ListCell<String> {
+public class HeroesListCell extends ListCell<Hero> {
 
     BorderPane cellLayout;
-    Label label;
+    Label heroIdLabel, heroNameLabel, heroDamageLabel;
 
     public HeroesListCell() {
         cellLayout = new BorderPane();
-        label = new Label();       
-        cellLayout.setCenter(label);
+        heroIdLabel = new Label();
+        heroNameLabel = new Label();
+        heroDamageLabel = new Label();
+        cellLayout.setLeft(heroIdLabel);
+        cellLayout.setCenter(heroNameLabel);
+        cellLayout.setRight(heroDamageLabel);
     }
 
     public Node getListCell() {
         return cellLayout;
     }
 
-    public void updateCell(String hero) {
-        label.setText(hero);
+    public void updateCell(Hero hero) {
+        heroIdLabel.setText(hero.gerHeroId());
+        heroNameLabel.setText(hero.getHeroName());
+        heroDamageLabel.setText(" " + hero.getHeroInitDamage());
     }
 
     @Override
-    protected void updateItem(String item, boolean empty) {
+    protected void updateItem(Hero item, boolean empty) {
         super.updateItem(item, empty);
         if (empty) {
             setGraphic(null);
